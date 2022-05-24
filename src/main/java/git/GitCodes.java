ae1039cf71eb12856ed6bedd5c3882c4f02e3cfb
hamitmizrak <hamitmizrak@gmail.com>
2022-05-24 21:26:52 +0300

    git codes all

---
+++ src/main/java/git/GitCodes.java
@@ -0,0 +1,15 @@
+package git;
+
+public class GitCodes {
+   /*
+    git init
+    git add .
+    git commit -m "first commit"
+    git push -u origin master
+
+    git status
+    git log
+    git branch
+    */
+
+}
