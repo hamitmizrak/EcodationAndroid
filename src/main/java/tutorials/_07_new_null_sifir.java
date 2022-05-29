fa6a4580f41630173443fea2fb23b0fdcd851da3
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-05-29 21:38:33 +0300

    new null sıfır

---
+++ src/main/java/tutorials/_07_new_null_sifir.java
@@ -0,0 +1,11 @@
+package tutorials;
+
+public class _07_new_null_sifir {
+    public static void main(String[] args) {
+        //new: hafıza(Ram) bir yer oluşturmak
+
+        //null: var olan hafızaki yeri boşaltmak
+
+        //sıfır: hafızada bir yer kaplıyor ancak değeri sıfırdır
+    }
+}
