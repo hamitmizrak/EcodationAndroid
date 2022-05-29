4dcc4e486ce3f7d55ceee8707694cf149d27c519
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-05-29 21:09:11 +0300

    Variable

--- src/main/java/tutorials/_02_Variable.java
+++ src/main/java/tutorials/_02_Variable.java
@@ -3,20 +3,7 @@ package tutorials;
 public class _02_Variable {
         //    psvm ==> TAB
     public static void main(String[] args) {
-        // 8 tane primitive type vardır:
-       //tam sayılar
-        byte b1 = 127;
-        short s1 =128;
-        int i1=5815;
-        long l1=645114L;
 
-        //virgüllü sayılar
-        float f2=452.55f;
-        double d2=15.52;
-
-        boolean b3=true;
-        char c3='@';
-        System.out.println(c3);
 
         //variable rules
         int sayi=5115;
