21b7de5624694be7ac0b8a7e4b2fca96bce3e538
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-05-29 21:38:09 +0300

    BestPractice

---
+++ src/main/java/tutorials/_00_BestPractice.java
@@ -0,0 +1,12 @@
+package tutorials;
+
+public class _00_BestPractice {
+    public static void main(String[] args) {
+
+        int telefon1=1122334455;
+        System.out.println(telefon1);
+
+        int telefon2=11_22_33_44_55;
+        System.out.println(telefon2);
+    }
+}
