8bbaa40cb2470d5e65ed59e4af00bc41bb7514fa
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-05-29 21:39:02 +0300

    Hata Turleri

---
+++ src/main/java/tutorials/_08_HataTurleri.java
@@ -0,0 +1,16 @@
+package tutorials;
+
+public class _08_HataTurleri {
+    public static void main(String[] args) {
+        //syntax error
+        //intx sayi=55;
+
+        //logical error
+        int sayi=5/0;
+        System.out.println(sayi);
+
+        //runtime error
+
+        //compiler error
+    }
+}
