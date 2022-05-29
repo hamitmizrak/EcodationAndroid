bdb6c90d086ec90f6b7ec4410df7ec306eff4ac6
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-05-29 21:09:34 +0300

    Primitive

---
+++ src/main/java/tutorials/_05_PrimitiveTypes.java
@@ -0,0 +1,23 @@
+package tutorials;
+
+public class _05_PrimitiveTypes {
+    public static void main(String[] args) {
+        // 8 tane primitive type vardır:
+        //tam sayılar
+        byte b1 = 127;
+        short s1 =128;
+        int i1=5815;
+        long l1=645114L;
+
+        //virgüllü sayılar
+        float f2=452.55f;
+        double d2=15.52;
+
+        boolean b3=true;
+        char c3='\u8532';
+        System.out.println(c3);
+
+        double bilimselGosterim=42E+5;
+        System.out.println(bilimselGosterim);
+    }
+}
