125861500b958f995d9d459a5950c73ff31189f2
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-05-29 20:51:14 +0300

    Common Rules

---
+++ src/main/java/tutorials/_03_CommonRules.java
@@ -0,0 +1,13 @@
+package tutorials;
+
+public class _03_CommonRules {
+    /*
+    interface ==> I ile başlar
+    metotlar  ==> camelCase yazılır
+    classslar ==> PascalCase yazılır.
+    paketler  ==> herpsi küçük harfle yazılır.
+    paketler  ==> unique (eşşiz demektir)
+    Const     ==> BUYUK_HARFLERLE_YAZILIR
+    değişkenler: camelCase
+     */
+}
