ac39d9014dc15d6470a477281ff5d5292fc820ac
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-05-29 21:39:16 +0300

    Örnekler

---
+++ src/main/java/tutorials/_09_SayiSistemleri.java
@@ -0,0 +1,25 @@
+package tutorials;
+
+public class _09_SayiSistemleri {
+    public static void main(String[] args) {
+
+        //2-8-16 ==> 10'luk tabanda karşılığı
+
+        //binary: ikilik : 0 -1
+        int binary=0b000110;
+        System.out.println(binary);
+
+        //octal: sekizlik: 0-1-2-3-4-5-6-7
+        int octal=0725302;
+        System.out.println(octal);
+
+        //decimal: onluk: 0-1-2-3-4-5-6-7-8-9
+        int decimal=123456;
+        System.out.println(decimal);
+
+        //onaltılık: onaltılık: 0-1-2-3-4-5-6-7-8-9-a-b-c-d-e-f
+        int hexadecimal=0xab52c;
+        System.out.println(hexadecimal);
+
+    }
+}
---
+++ src/main/java/tutorials/_10_Examples.java
@@ -0,0 +1,30 @@
+package tutorials;
+
+public class _10_Examples {
+    public static void main(String[] args) {
+        //örnek-1: 1. 3 bilinmeyenli denklem
+        //z=4x-9y
+        //x=4
+        //y=1
+        //z=?
+
+        int x,y,z;
+        x=4;
+        y=1;
+        z=4*x-9*y;
+        System.out.println(z);
+
+        //örnek-2: Dereceyi Fahranhayt çevirelim
+        //10 derece kaç fahranhayttır ?
+        //Dereceyi fahranhayta çevirme formülü => (derece*9)/5+32
+
+        int derece,fahrenhayt;
+        derece=10;
+        fahrenhayt=(derece*9)/5+32;
+        System.out.println(fahrenhayt);
+
+        //örnek-3: 5+6*2/2+62-9 ==> Java ile hesaplıyalım ?
+
+
+    }
+}
