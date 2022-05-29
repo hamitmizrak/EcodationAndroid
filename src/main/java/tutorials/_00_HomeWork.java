5dab189a9de0d44eac7e6e8ceb15bbbb41d8d919
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-05-29 20:50:29 +0300

    HomeWork

---
+++ src/main/java/tutorials/_00_HomeWork.java
@@ -0,0 +1,31 @@
+package tutorials;
+
+public class _00_HomeWork {
+
+    /*
+    1.Araştırma Ödevi:
+    iyi bir commit özellikleri nelerdir ?
+    Java özellikleri nelerdir ?
+    JDK nedir ?
+    JVM nedir ?
+    JRE nedir ?
+    interpreter nedir ?
+    compiler nedir ?
+    Java 100/100 oop midir ?
+    heap memory nedir ?
+    stack memory nedir ?
+
+    2.Araştırma Ödevi
+    OOP nedir ?
+    open source nedir ?
+    API nedir ?
+    IDE nedir ?
+    JIT nedir ?
+    Senkron nedir ?
+    Asenkron nedir ?
+
+    Primitive Type nedir ?
+    Wrapper Type nedir ?
+   Primitive Type - Wrapper Type  aralarındaki farklar nelerdir ?
+     */
+}
