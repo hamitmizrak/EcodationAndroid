ddd224b27c21beedd61e4a8e44a2201de395eb00
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-06-04 20:29:35 +0300

    Escape Character

---
+++ src/main/java/tutorials/_14_EscapeCharacter.java
@@ -0,0 +1,12 @@
+package tutorials;
+
+import javax.swing.*;
+
+public class _14_EscapeCharacter {
+    public static void main(String[] args) {
+        System.out.println("\"");
+        System.out.println("1.satýr\n2.satýr");
+        System.out.println("1.satýr\n\t2.satýr");
+
+    }
+}
