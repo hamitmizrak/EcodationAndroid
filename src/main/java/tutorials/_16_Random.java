3331756cc066089cce69113c39bb3e78dba2d002
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-06-04 21:04:44 +0300

    Random

---
+++ src/main/java/tutorials/_16_Random.java
@@ -0,0 +1,23 @@
+package tutorials;
+
+import java.util.Random;
+
+public class _16_Random {
+    public static void main(String[] args) {
+
+        //Math
+        System.out.println("Rastgele sayý: "+ Math.round(Math.random()*4+1));
+
+        //Object
+        Random random=new Random();
+        int sayi=random.nextInt(4)+1;
+        System.out.println("Rastgele sayý: "+sayi);
+
+        Random random2=new Random();
+        //10 tane rastgele sayý olsun
+        for (int i = 1; i <=10 ; i++) {
+            int sayi2=random2.nextInt(4)+1;
+            System.out.print(sayi2+" ");
+        }
+    }
+}
