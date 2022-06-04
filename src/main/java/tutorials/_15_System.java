4ce1c5ab4e8c558f8e9b0d94a2b9e93459a69a8a
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-06-04 20:32:55 +0300

    print , println

---
+++ src/main/java/tutorials/_15_System.java
@@ -0,0 +1,12 @@
+package tutorials;
+
+public class _15_System {
+    public static void main(String[] args) {
+        //System.in//input
+        //System.out//output
+        System.out.print("Bir sonraki gelen alt satýra geçecek");
+        System.out.println("\txxxx");
+        System.out.println("yyyy");
+
+    }
+}
