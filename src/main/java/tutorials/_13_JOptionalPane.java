9fdae3c1c8efe3963c04f27cb5753f16b5cb5548
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-06-04 20:03:02 +0300

    OptionalPane

---
+++ src/main/java/tutorials/_13_JOptionalPane.java
@@ -0,0 +1,11 @@
+package tutorials;
+
+import javax.swing.*;
+
+public class _13_JOptionalPane {
+    public static void main(String[] args) {
+
+        String secondPath= JOptionPane.showInputDialog("Lütfen birþeyler yazýnýz");
+        System.out.println(secondPath);
+    }
+}
