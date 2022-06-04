a413b68fca9392d606c31f19cbfe8c6dad2b66bd
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-06-04 20:59:23 +0300

    Math Ödev

---
+++ src/main/java/tutorials/_16_Math.java
@@ -0,0 +1,25 @@
+package tutorials;
+
+public class _16_Math {
+    public static void main(String[] args) {
+        //Mathematics: static
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+        System.out.println("Karekök: "+Math.sqrt(16));
+        System.out.println("Mutlak Deðer: "+Math.abs(-4));
+        System.out.println("En küçük sayý: "+Math.min(3,-99));
+        System.out.println("En büyük sayý: "+Math.max(3,-99));
+        System.out.println("Üslü sayýlar: "+Math.pow(2,5));
+        System.out.println("Rastgele sayý: "+Math.random()*4+1);
+
+        System.out.println("Aþaðý yuvarlamak sayý: "+Math.floor(2.9));
+        System.out.println("Yuvarý yuvarlamak sayý: "+Math.ceil(2.1));
+        System.out.println("Round: "+Math.round(5.4)); //x<=4 ve aþaðýdaki aþaðý yuvarlamak
+        System.out.println("Round: "+Math.round(5.5)); //x>=5 ve yukarý  yuvarlamak
+        //ÖDEV Math sýnýfý kullanarak kullanýcýdan alýnan iki sayýnýn 4 iþlemini yapýlacak, Hesap makinesi ?
+        //Scanner
+        //+ - * /
+
+    }
+}
