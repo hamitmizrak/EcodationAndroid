295866b4c2bbd308f70031b39b0b9fefede04713
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-06-04 20:34:56 +0300

    System.err.println("Deneme")

--- src/main/java/tutorials/_15_System.java
+++ src/main/java/tutorials/_15_System.java
@@ -4,9 +4,13 @@ public class _15_System {
     public static void main(String[] args) {
         //System.in//input
         //System.out//output
+        //System.gc() //garbarage collection
+        //System.exit(0)
         System.out.print("Bir sonraki gelen alt satýra geçecek");
         System.out.println("\txxxx");
         System.out.println("yyyy");
+        System.err.println("Hata mesajý");
+
 
     }
 }
