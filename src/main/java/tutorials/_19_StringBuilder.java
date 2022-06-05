6f939d8e5fd194a72b98589b6a46833e7b2c6d86
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-06-05 19:48:35 +0300

    StringBuilder

--- src/main/java/tutorials/_19_StringBuilder.java
+++ src/main/java/tutorials/_19_StringBuilder.java
@@ -4,7 +4,36 @@ package tutorials;
 public class _19_StringBuilder {
     public static void main(String[] args) {
 
-     //compiler ile interper
+         //Builder birleþtirme
+        //String birleþtirmesi
+
+        String adi="Hamit",soyadi="Mýzrak";
+
+        //1-)birleþtirme
+        String birlestirme1=adi+soyadi;
+        System.out.println(birlestirme1);
+
+        System.out.println("***************************");
+
+        //2-)birleþtirme
+        String birlestirme2=adi.concat(soyadi);
+        System.out.println(birlestirme2);
+
+        System.out.println("***************************");
+
+        //3-)birleþtirme
+        StringBuilder birlestirme3=new StringBuilder();
+        birlestirme3.append(adi).append(soyadi);
+        String stringTo=birlestirme3.toString();
+        System.out.println(stringTo);
+
+        System.out.println("***************************");
+
+        //4-)birleþtirme
+        StringBuffer birlestirme4=new StringBuffer();
+        birlestirme4.append(adi).append(soyadi);
+        String stringTo2=birlestirme4.toString();
+        System.out.println(stringTo2);
 
 
 
