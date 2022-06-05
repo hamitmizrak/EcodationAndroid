1edde9d1c083969efac88125f98ced6ff128ea21
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-06-05 19:48:46 +0300

    StringTokenizer

--- src/main/java/tutorials/_20_StringTokenizer.java
+++ src/main/java/tutorials/_20_StringTokenizer.java
@@ -1,10 +1,25 @@
 package tutorials;
 
 
+import java.util.StringTokenizer;
+
 public class _20_StringTokenizer {
     public static void main(String[] args) {
 
+        String data="Servlet jsp jsf primefaces spring boot";
 
+        //1-) parçalama
+        String[] dizi=data.split(" ");
+        for(String temp:dizi){
+            System.out.println(temp);
+        }
 
+        System.out.println("**********************************");
+
+        //2-) parçalama
+        StringTokenizer tokenizer=new StringTokenizer(data," ");
+        while(tokenizer.hasMoreTokens()){
+            System.out.println(tokenizer.nextToken());
+        }
     }
 }
