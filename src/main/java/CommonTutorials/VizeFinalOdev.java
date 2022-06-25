c4969126bc1845ab27ee1edcb2392187d13ee6ce
hamitmizrak <hamitmizrak@gmail.com>
2022-06-25 21:50:28 +0300

    Ödev vize final

---
+++ src/main/java/CommonTutorials/VizeFinalOdev.java
@@ -0,0 +1,24 @@
+package CommonTutorials;
+
+public class VizeFinalOdev {
+
+// kullanýcýdan alýnan vize ve final notuna göre geçme(ortalama) Algoritmasý
+
+// eðer kullanýcý vize veya final 0(sýfýr) bir deðer girerse sistemden atsýn
+// ekranda "Sifir disiplin hatasi Sistemden cikiliyor"
+// bunun haricinde sürekli vize final sorsun
+
+// Not Harfleri ==> AA BA BB FF
+// not ortalamasý: ortalama<50 altýnda ise kaldý FF
+// not ortalamasý: ortalama==50 Geçti
+// not ortalamasý: 55<=x<=69 BB
+// not ortalamasý: 70<=x<=84 BA
+// not ortalamasý: 85<=x<=100 AA
+
+// Vize:40% Final:60%
+// Dikkat: Consolda virgül kullanmalýsýn.
+
+// while sonsuz döngü   ==> while(true) {}
+// for sonsuz döngü     ==> for(;;) {}
+
+}
