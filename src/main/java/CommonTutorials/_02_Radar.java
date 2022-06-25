ad46ee7dec5192fa397feb4af8af9eca5f24c8c6
hamitmizrak <hamitmizrak@gmail.com>
2022-06-25 21:22:51 +0300

    Radar

--- src/main/java/CommonTutorials/_02_Radar.java
+++ src/main/java/CommonTutorials/_02_Radar.java
@@ -11,38 +11,43 @@ import java.util.Scanner;
 // ve eðer toplam ceza sayýsý 5 ve üstüne ise ehliyetine el konulsun  hesaplayan algoritma yazalým ?
 public class _02_Radar {
 
-private static int AMOUNT=0;
-private static int HOW_MANY=0;
-    public static void radar() {
-        Scanner klavye = new Scanner(System.in);
-        System.out.println("Hýzýnýzý giriniz");
-        int speed = klavye.nextInt();
+    private static int AMOUNT = 0;
+    private static int HOW_MANY = 0;
 
-        if(HOW_MANY>=5){
+
+    public static int dataScanner() {
+        Scanner klavye = new Scanner(System.in);
+        System.out.println("++++++++++++++\nHýzýnýzý giriniz");
+        int data = klavye.nextInt();
+        return data;
+    }
+
+    public static void radar() {
+        int speed = dataScanner();
+
+        if (HOW_MANY >= 5) {
             System.out.println("Ehliyetine el konuldu");
             System.exit(0);
             //return;
-        }else{
-            if(80<=speed && speed<=94){
-                AMOUNT=AMOUNT+800;
+        } else {
+            if (80 <= speed && speed <= 94) {
+                AMOUNT = AMOUNT + 800;
                 HOW_MANY++;
-                System.out.println("+++++++++\nHýzýnýz: "+speed+" yavaþlayýn:\npara cezasý:800TL \ntoplam para cezanýz: "+AMOUNT+"\ntoplam ceza sayýsý: "+HOW_MANY);
-            }
-            else if(95<=speed && speed<=119){
-                AMOUNT+=1200;
+                System.out.println("+++++++++\nHýzýnýz: " + speed + " yavaþlayýn:\npara cezasý:800TL \ntoplam para cezanýz: " + AMOUNT + "\ntoplam ceza sayýsý: " + HOW_MANY);
+            } else if (95 <= speed && speed <= 119) {
+                AMOUNT += 1200;
                 HOW_MANY++;
-                System.out.println("+++++++++\nHýzýnýz: "+speed+" yavaþlayýn:\npara cezasý:1200TL \ntoplam para cezanýz: "+AMOUNT+"\ntoplam ceza sayýsý: "+HOW_MANY);
-            }
-            else if(speed>=120){
-                AMOUNT+=2000;
+                System.out.println("+++++++++\nHýzýnýz: " + speed + " yavaþlayýn:\npara cezasý:1200TL \ntoplam para cezanýz: " + AMOUNT + "\ntoplam ceza sayýsý: " + HOW_MANY);
+            } else if (speed >= 120) {
+                AMOUNT += 2000;
                 HOW_MANY++;
-                System.out.println("+++++++++\nHýzýnýz: "+speed+" yavaþlayýn:\npara cezasý:2000TL \ntoplam para cezanýz: "+AMOUNT+"\ntoplam ceza sayýsý: "+HOW_MANY);
+                System.out.println("+++++++++\nHýzýnýz: " + speed + " yavaþlayýn:\npara cezasý:2000TL \ntoplam para cezanýz: " + AMOUNT + "\ntoplam ceza sayýsý: " + HOW_MANY);
             }
         }
     }
 
     public static void main(String[] args) {
-        while(true){
+        while (true) {
             radar();
         }
     }
