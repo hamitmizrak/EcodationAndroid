8dc87736caf1b9e40e80081aa7a2bd1219a5b076
hamitmizrak <hamitmizrak@gmail.com>
2022-06-26 20:31:29 +0300

    String tersten yazdırmak

--- src/main/java/CommonTutorials/TerstenYazOdev.java
+++ src/main/java/CommonTutorials/TerstenYazOdev.java
@@ -1,8 +1,25 @@
 package CommonTutorials;
 
+
+import java.util.Scanner;
+
 public class TerstenYazOdev {
 
     // kullanýcýdan alýnan veriyi tersten yazdýran algoritma ?
     // Hamit
     // timaH
+    public static void reverseString() {
+        Scanner klavye = new Scanner(System.in);
+        System.out.println("Bir þey yazýnýz");
+        String value = klavye.nextLine();
+        String temp = "";
+        for (int i = value.length() - 1; i >= 0; i--) {
+            temp += value.charAt(i);
+        }
+        System.out.println(temp);
+    }
+
+    public static void main(String[] args) {
+        reverseString();
+    }
 }
