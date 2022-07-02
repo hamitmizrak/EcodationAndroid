84b65a44e49d89ffbe9df6ead0379ec013cf9534
hamitmizrak <hamitmizrak@gmail.com>
2022-07-02 20:04:51 +0300

    Generics Type constructor method

--- src/main/java/datastructure/GenericsTuto.java
+++ src/main/java/datastructure/GenericsTuto.java
@@ -8,10 +8,25 @@ package datastructure;
 // casting (dönüþüm) iþlem yazpmamýza gerek kalmaz.
 // Karþý taraftan hangi veri geleceðini bilmiyorsak bu bizim için önemli bir yapýdýr ve bize yardýmcý olur.
 
-public class GenericsTuto <T>  {
-private T value;
+public class GenericsTuto<T> {
+    private T value;
 
-//getter and setter
+    //parametresiz constructor
+    public GenericsTuto() {
+
+    }
+
+    //generics parametreli constructor
+    public GenericsTuto(T value) {
+        this.value = value;
+    }
+
+    //generics method
+    public <K> void genericsMethod(K value){
+        System.out.println(value);
+    }
+
+    //getter and setter
     public T getValue() {
         return value;
     }
@@ -22,8 +37,10 @@ private T value;
 
     //PSVM
     public static void main(String[] args) {
-        GenericsTuto tuto=new GenericsTuto();
+        GenericsTuto tuto = new GenericsTuto(true);
+        System.out.println(tuto.getValue());
         tuto.setValue(55.63);
         System.out.println(tuto.getValue());
+        tuto.genericsMethod(true);
     }
 }
