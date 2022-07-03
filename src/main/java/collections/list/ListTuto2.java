6e72302303ea6e1a68d8e76dce21a461dbe73e15
hamitmizrak <hamitmizrak@gmail.com>
2022-07-03 19:32:33 +0300

    Set

--- src/main/java/collections/list/ListTuto2.java
+++ src/main/java/collections/list/ListTuto2.java
@@ -9,9 +9,9 @@ package collections.list;
 import java.util.*;
 
 public class ListTuto2 {
-    public static  void listMethod(){
+    public static void listMethod() {
         //List<String> iller=new ArrayList<>();
-        List<String> iller=new LinkedList<>();
+        List<String> iller = new LinkedList<>();
 
         //add() ==> listeye veri eklemeye yarar.
         iller.add("Malatya");
@@ -22,8 +22,8 @@ public class ListTuto2 {
 
         System.out.println("******Iterator döngü*************");
         //3-) Iterator döngü
-        Iterator iterator= iller.iterator();
-        while(iterator.hasNext()){
+        Iterator iterator = iller.iterator();
+        while (iterator.hasNext()) {
             System.out.println(iterator.next());
         }
 
@@ -31,34 +31,34 @@ public class ListTuto2 {
         //Collections.sort(liste): Küçükten Büyüðe sýralama
         java.util.Collections.sort(iller);
         System.out.println("******Collections.sort(liste) ==> Küçükten Büyüðe*************");
-        for(String temp :iller){
+        for (String temp : iller) {
             System.out.println(temp);
         }
 
         //Collections.reverse(liste): Büyükten Küçüðe sýralama
         java.util.Collections.reverse(iller);
         System.out.println("******Collections.reverse() ==> Büyükten Küçüðe*************");
-        for(String temp :iller){
+        for (String temp : iller) {
             System.out.println(temp);
         }
 
         //Collections.shuffle(liste: Karýþýk
         java.util.Collections.shuffle(iller);
         System.out.println("******Collections.reverse(): Karýþýk*************");
-        for(String temp :iller){
+        for (String temp : iller) {
             System.out.println(temp);
         }
 
         //unmodifiableList: listeye artýk yeni veriler eklenmesini istemiyorsak
         //Exception: Exception in thread "main" java.lang.UnsupportedOperationException
-        iller= java.util.Collections.unmodifiableList(iller);
+        iller = java.util.Collections.unmodifiableList(iller);
         iller.add("eklenmez");
-        for(String temp :iller){
+        for (String temp : iller) {
             System.out.println(temp);
         }
 
         System.out.println("******Collections.binarySearch(liste,kelime): *************");
-        System.out.println(java.util.Collections.binarySearch(iller,"Elazýð"));
+        System.out.println(java.util.Collections.binarySearch(iller, "Elazýð"));
     }
 
     //PSVM
